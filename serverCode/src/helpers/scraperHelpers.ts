d8dd4dfa8bb634e362f17dd11b618ff8a020b11a
ahmedjw <ahmedvander@gmail.com>
2024-07-05 17:01:55 +0300

    build functionailty for scraping sorrties websites

---
+++ serverCode/src/helpers/scraperHelpers.ts
@@ -0,0 +1,51 @@
+export async function websitesScraperConfigrations(website: string, page: any) {
+  switch (website) {
+    //scraper configration for mcommunity website
+    case "mcommunity": {
+      const selectElement = await page.$('label[name="page-size"] select');
+      if (selectElement) await selectElement.click();
+      await delay(1000);
+      await page.select('label[name="page-size"] select', "500");
+      await page.keyboard.press("ArrowDown");
+      await page.keyboard.press("ArrowDown");
+      await page.keyboard.press("ArrowDown");
+      await page.keyboard.press("Enter");
+      delay(2000);
+      break;
+    }
+    //scraper configration for static websites and lazyloading websites
+    default:
+      await autoScroll(page);
+  }
+}
+
+export async function autoScroll(page: any) {
+  const getDocumentHeight = async () => {
+    return await page.evaluate(() => document.body.scrollHeight);
+  };
+
+  let previousHeight = await getDocumentHeight();
+  let currentHeight;
+
+  while (true) {
+    // Scroll to the bottom of the page
+    await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+
+    // Wait for new content to load
+    await delay(3000);
+
+    currentHeight = await getDocumentHeight();
+
+    // If the height of the document hasn't changed, break the loop
+    if (currentHeight === previousHeight) {
+      break;
+    }
+    previousHeight = currentHeight;
+  }
+}
+
+export async function delay(time: any) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
--- serverCode/src/services/scraperService.ts
+++ serverCode/src/services/scraperService.ts
@@ -1,24 +1,51 @@
-import axios from "axios";
 import cheerio from "cheerio";
+import { autoScroll, delay } from "../helpers/scraperHelpers";
+import { websitesScraperConfigrations } from "../helpers/scraperHelpers";
+import puppeteer from "puppeteer";
 
 const scraperService = {
-  //basic Scraper until I get properly inormaitons
-  async scrape(parameter: string): Promise<any> {
-    const url = "https://www.skool.com/";
+  async scrollAndScrape(url: string) {
+    const emailsSet = new Set();
+    const browser = await puppeteer.launch({
+      // headless: false, // used to visualize the process in chrome
+    });
+    const page = await browser.newPage();
     try {
-      const response = await axios.get(url);
-      const $ = cheerio.load(response.data);
+      await page.goto(url, { waitUntil: "networkidle2", timeout: 300000 });
+      await delay(3000);
+      if (url.includes("mcommunity"))
+        websitesScraperConfigrations("mcommunity", page);
+      else await autoScroll(page);
 
-      // Example: Extract data from HTML using cheerio
-      const title = $("title").text();
-      const metaDescription = $('meta[name="description"]').attr("content");
+      await delay(2000);
+      const htmlContent = await page.content();
+      await delay(2000);
+      // Load HTML content into Cheerio
+      const $ = cheerio.load(htmlContent);
+      //email regex to find emails
+      const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+      // Get all text content from the page
+      const textContent = $.html();
+      // Find all email addresses in the text content
+      const emails = textContent.match(emailRegex);
 
-      return { title, metaDescription };
+      emails?.forEach((item:string) => {
+        emailsSet.add(item);
+      });
+
+      return { emailsSet };
     } catch (error) {
-      console.error("Error scraping:", error);
-      throw new Error("Failed to scrape data");
+      console.error("Error:", error);
+    } finally {
+      await browser.close();
     }
   },
 };
 
 export default scraperService;
+
+
+// scraperService.scrollAndScrape("https://stanfordwho.stanford.edu/people?sort=20&keyword=eng");
+// scraperService.scrollAndScrape(
+//   "https://mcommunity.umich.edu/?value=eng&base=all"
+// );
