eba617e191dc028d9136a24ee34acaebf49d18bf
ahmedjw <ahmedvander@gmail.com>
2024-12-25 12:22:32 +0200

    remove footer from layout

--- clientCode/src/components/Layout.tsx
+++ clientCode/src/components/Layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import NavBar from "./navBar";
 import { Outlet } from "react-router-dom";
 import { contentStyle } from "./navBar/Style";
-import Footer from "./footer";
 
 export const Layout: React.FC = () => {
   return (
@@ -11,8 +10,6 @@ export const Layout: React.FC = () => {
       <div style={contentStyle}>
         <Outlet />
       </div>
-      {/* <Footer /> */}
-      {/* // instead of building a footer component */}
     </>
   );
 };
